be39098065004f6ce1201704dfb3f37fe0630417
franciscomtz0719 <franciscomtz0719@gmail.com>
2022-07-15 20:11:52 -0500

    last changes

--- js/login.js
+++ js/login.js
@@ -1,7 +1,39 @@
-const API_URL = "http://localhost:8080/login"
+const API_URL = "http://localhost:8080/users"
+const login_url = "http://localhost:8080/login"
 const buttonLogin = document.querySelector("#login__button")
+const buttonSignin = document.querySelector("#signin__button")
 const emailInput = document.querySelector("#typeEmail")
 const passwordlInput = document.querySelector("#typePassword")
+
+buttonSignin.addEventListener("click", async (event) =>{
+    console.log("prueba click")
+    event.preventDefault()
+    const email = emailInput.value
+    const password = passwordlInput.value
+
+    const dataBackend = {
+        email,
+        password
+    }
+
+    fetch(API_URL, {
+        method: 'POST',
+        body: JSON.stringify(dataBackend),
+        headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        }) 
+        .then((response) => {
+            console.log(response, "Si entro al fetch de new comment")
+            return response.json()
+        })
+        .then((finalResponse) => {
+            alert (`Usuario creado con exito!`)
+        })
+
+} )
+
+
 
 buttonLogin.addEventListener("click", async (event) =>{
     event.preventDefault()
@@ -13,7 +45,7 @@ buttonLogin.addEventListener("click", async (event) =>{
         password
     }
 
-    const data = await fetch(API_URL,  {
+    const data = await fetch(login_url,  {
         method: "Post",
         headers: {
             "Content-Type" : "application/json"
@@ -22,9 +54,13 @@ buttonLogin.addEventListener("click", async (event) =>{
     })
 
     const response = await data.json()
-    const token = response.token
-
+    const token = response?.token
+    console.log (token) 
+    if (!token){
+        window.alert("Invalid Password")
+        return
+    }
     localStorage.setItem("token", token)
     window.location.href = "/index.html"
-
+    
 })
